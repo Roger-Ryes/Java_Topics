dae64a9959464392b8af327e3e3e6f088fc0454a
Roger-Ryes <roger.reyesm97@gmail.com>
2022-04-15 21:28:18 -0500

    Compilacion y ejecucion por Consola

---
+++ 11_Compilacion/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Ejecutando test " + args.length);
+        for(String s: args){
+            System.out.println(s);
+        }
+    }
+}
